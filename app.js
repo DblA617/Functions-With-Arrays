55a2f795f91624198941b9417e384d2db420a412
Aarika Turner <turneraarika@gmail.com>
2021-02-04 16:51:22 -0500

    push

---
+++ app.js
@@ -0,0 +1,10 @@
+var newNew = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+newNew.push([4, " is a lucky number"]);
+
+console.log (newNew);
+
+let crayArray = ["This", "That", "Cow", "Boy",];
+let crayArr = crayArray.splice(1,2);
+
+console.log(crayArr);
+
